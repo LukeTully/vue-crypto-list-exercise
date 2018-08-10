83083eba50bdbda5c193433337330e424874471e
Luke <martinlm@umanitoba.ca>
2018-08-10 14:58:00 -0700

    Remove unused AssetGenerator class

--- src/ApiMock.js
+++ src/ApiMock.js
@@ -17,68 +17,6 @@ var ExchangeRatesToCAD = [
   new ExchangeRate("ETH", 500.12717)
 ];
 
-
-const acceptedTypes = ["icons"];
-
-class AssetGenerator {
-  constructor(assetDir, options) {
-    /* 
-		
-		assetDir: Subdirectory of all static assets
-		options: Dictionary of specific asset type subdirectories (e.g assets/icons)
-		
-		*/
-    this.root = assetDir;
-    this.types = this.filterOptions(options);
-  }
-  getAcceptedTypes() {
-    return acceptedTypes;
-  }
-  filterOptions(options) {
-    let types = this.getAcceptedTypes();
-	let filteredOptions = {};
-    // Filter user provided options by our internal list of accepted options
-    types.map(typeKey => {
-    	filteredOptions[typeKey] = options[typeKey];
-	});
-	return filteredOptions;
-  }
-  getAssetTypes() {
-    return this.types;
-  }
-
-  // TODO: Refactor so it makes sense given the migration to a seperate class
-  GetStaticIcons(currencyList) {
-	var _self = this;
-    return new Promise(function(resolve, reject) {
-      setTimeout(function() {
-        if (Math.random() < 0.2) {
-          reject("Could not connect to server");
-          return;
-        }
-		let icons = {};
-        currencyList = currencyList.map(wallet => {
-		  
-		  console.log(this);
-		  let assetTypes = _self.getAssetTypes();
-
-          // Construct fully qualified asset paths
-          if (assetTypes["icons"]) {
-            // TODO: Should not be static. Abstract away with getter
-            icons[wallet.currency] = `${
-              assetTypes["icons"]
-            }${wallet.currency.toLowerCase()}.svg`;
-          } else {
-            icons[wallet.currency] = `${wallet.currency.toLowerCase()}.svg`;
-          }
-        });
-
-        resolve(icons);
-      }, 250);
-    });
-  }
-}
-
 function GetWallets() {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
