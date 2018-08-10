d3df6777cec4a3dd01fd1c45ccdead92382f282a
Luke <martinlm@umanitoba.ca>
2018-08-10 14:52:13 -0700

    Improve function descriptions based on JSDoc3 standards

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,49 @@
 /*   
+
   Rounds a number to the nearest nth decimal point
   Pulled from https://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-only-if-necessary
+
+  @return
+    The value of num rounded to the value of decimalCount
+
+  @param {Number} num
+    Number to round
+  
+  @param {Number} decimalCount
+    Number of decimal places to round num to
+
 */
-function roundToX(num, decimals) {
-  return +(Math.round(num + `e+${decimals}`) + `e-${decimals}`);
+function roundToX(num, decimalCount) {
+  return +(Math.round(num + `e+${decimalCount}`) + `e-${decimalCount}`);
 }
 
+/*
+
+  Converts the input number to USD using default optional params can be included
+  according to the Intl.NumberFormat spec
+
+  @return
+    The value of num converted to a currency string based on the optional params
+
+  @param {Number} num
+    number to convert to currency
+
+  @param {String} locale
+    local language tag that the currency format should be returned in
+
+  @param {object} options
+    Other config options according to spec
+
+*/
 function convertToCurrency(num, locale = "en-US", options = {
   style: "currency",
   currency: "USD"
 }) {
-  /* Formats an input number as US currency */
   let format = new Intl.NumberFormat(locale, options);
   return format.format(num);
 }
+
+
 export {
   roundToX as precision,
   convertToCurrency
